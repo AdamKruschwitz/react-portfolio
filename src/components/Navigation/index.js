74737dd4c3178dc22183edc4062a48893c7860b9
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-11-23 20:27:31 -0800

    fixed styling errors

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,22 @@
 import React from 'react';
-import { Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, } from 'react-bootstrap';
 
 function Navigation({}) {
     return (
-        <Nav 
-            activeKey="/aboutMe"
-            // onSelect=
-        >
-            <Nav.Item>
-                <Nav.Link href="#">Adam Kruschwitz</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#">About Me</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#">Portfolio</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#">Contact</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Button >Resume</Button>
-            </Nav.Item>
-        </Nav>
+    <Navbar bg="light" expand="lg">
+    <Container>
+        <Navbar.Brand>Adam Kruschwitz</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#aboutMe">About Me</Nav.Link>
+                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+                <Button href="#resume">Download Resume</Button>{' '}
+            </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     )
 }
 
